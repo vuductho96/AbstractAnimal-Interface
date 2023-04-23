b353e78e67cfd8c517f6da1378a6ca67bc0e65f8
vuductho96 <112962875+vuductho96@users.noreply.github.com>
2023-04-23 09:50:08 +0700

    code

---
+++ Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace ANIMAL
+{
+    public abstract class Animal
+    {
+        public abstract string MakeSound();
+
+
+
+    }
+    
+    public class Tiger : Animal
+    {
+        public override string MakeSound()
+        {
+            return "Tiger: roarrrrr!";
+        }
+    }
+    ///////////////////////////////////////Interface //////////////////////////
+
+    public interface Edible
+    {
+        public string HowToEat();
+
+
+    }
+    public abstract class Fruit : Edible
+    {
+        public abstract string HowToEat();
+    }
+    public class Chicken : Animal, Edible
+    {
+        public override string MakeSound()
+        {
+            return "Chicken: cluck-cluck!";
+        }
+
+        public string HowToEat()
+        {
+            return "could be fried";
+        }
+    }
+    public class Apple : Fruit
+    {
+        public override string HowToEat()
+        {
+            return "Apple could be slided";
+        }
+    }
+    public class Orange : Fruit
+    {
+        public override string HowToEat()
+        {
+            return "Orange could be juiced";
+        }
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal[] animals = new Animal[2];
+            animals[0] = new Tiger();
+            animals[1] = new Chicken();
+
+            foreach (Animal animal in animals)
+            {
+                Console.WriteLine(animal.MakeSound());
+            }
+            Fruit[] fruits = new Fruit[2];
+            fruits[0] = new Orange();
+            fruits[1] = new Apple();
+            foreach (Fruit fruit in fruits)
+            {
+                Console.WriteLine(fruit.HowToEat());
+            }
+        }
+    }
+}
+
+
+    
+
+
+
+
+
+
